a1ef08058b1d966ca917b2d690f8d1a6944d3cad
Luis <lagui@gmail.com>
2026-07-03 11:22:23 -0600

    Voy a hacer el menu

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 void main()
 {
-//Goyo me checas
-    //se sube
-    //GOYOOOO
+
+     int opcion = 0;
+    do
+    {
+        System.out.println("\n=============================================");
+        System.out.println("     REGISTRO DE USUARIO      ");
+        System.out.println("=============================================");
+
+
+
+
+
+
+    } while (true);
+
+
 }
