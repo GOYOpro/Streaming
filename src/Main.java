62001913c5f2d7fdfc393c5d55021eec3c97774e
Luis <lagui@gmail.com>
2026-07-03 11:50:59 -0600

    .

--- src/Main.java
+++ src/Main.java
@@ -6,35 +6,38 @@ import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
-
-void main()
+public class Main
 {
-
-     int opcion = 0;
-    do
+    static void main()
     {
-        System.out.println("\n=============================================");
-        System.out.println("     BASE DE DATOS - PLATAFORMA DE STREAMING     ");
-        System.out.println("=============================================");
 
-        System.out.println("1. Registrar usuario");
-        System.out.println("2. Mostrar usuarios");
-
-        if (opcion == 1)
-        {
-            System.out.println("Ingrese el correo");
-
-
-        }
-        else if (opcion == 2)
-        {
+        PlataformaStreaming miMetodo = new PlataformaStreaming();
+        int opcion = 0, meses = 0;
+        String correo = "";
 
 
 
-        }
+        do {
+            System.out.println("\n=============================================");
+            System.out.println("     BASE DE DATOS - PLATAFORMA DE STREAMING     ");
+            System.out.println("=============================================");
+
+            System.out.println("1. Registrar usuario");
+            System.out.println("2. Mostrar reportes");
+
+            if (opcion == 1)
+            {
+                //Se llama al metodo registrar
+
+           // miMetodo.AgregarUsuarios();
+            } else if (opcion == 2) {
+
+            miMetodo.Reporte();
+            }
 
 
-    } while (true);
+        } while (true);
 
 
+    }
 }
