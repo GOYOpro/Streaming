0780c6525187a2d8f2b626cf31eaa809b448e6c2
GOYOpro <jesusreye946@gmail.com>
2026-07-07 10:42:48 -0600

    Correcion ya añade y muestra reporte version 2

--- src/Main.java
+++ src/Main.java
@@ -15,16 +15,67 @@ public class Main
 
         PlataformaStreaming miMetodo = new PlataformaStreaming();
         Scanner sc = new Scanner(System.in);
-        int opcion = 0, meses = 0;
-        String opcion2 = "", correo = "";
-        miMetodo.AgregarUsuarios(new CuentaUsuario("David@783", 7, new PlanBasico()));
+        CuentaUsuario cuentaUsuario;
+        Subscripcion subscripcionElegida = null;
+        int opcion = 0,opcionPlan = 0, meses = 0,plan= 0;
+        String correo = "";
 
-        miMetodo.AgregarUsuarios(new CuentaUsuario("Jesus@9384", 5, new PlanEstandar()));
+        miMetodo.AgregarUsuarios(new CuentaUsuario("Jesus@",4,new PlanEstandar()));
+        try {
+            do {
+                System.out.println("1.Crear cuenta");
+                System.out.println("2.Usuarios existentes");
+                System.out.println("3.Reporte");
+                opcion = Integer.parseInt(sc.nextLine());
 
-        miMetodo.AgregarUsuarios(new CuentaUsuario("Adrian@", 5, new PlanPremium()));
+                switch (opcion){
+                    case 1:
+                        System.out.println("========DATOS PARA INGRESAR DEL USUARIO=======");
+                        System.out.println("Ingrese un correo");
+                        correo = sc.nextLine();
 
-        miMetodo.Reporte();
+                        System.out.println("Selecciona el tipo de subcripcion");
+                        System.out.println("1.Plan Basico");
+                        System.out.println("2.Plan Estandar");
+                        System.out.println("3.Plan Premium");
+                        plan = sc.nextInt();
 
+                        System.out.println("Igrese los meses activo");
+                        meses = sc.nextInt();
+                        sc.nextLine();
+
+                        switch (plan){
+                            case 1:
+                                subscripcionElegida  = new PlanBasico();
+                                break;
+                            case 2:
+                                subscripcionElegida = new PlanEstandar();
+                                break;
+                            case 3:
+                                subscripcionElegida = new PlanPremium();
+                                break;
+                            default:
+                        }
+
+                        cuentaUsuario = new CuentaUsuario(correo,meses,subscripcionElegida);
+                        miMetodo.usuarios.add(cuentaUsuario);
+                        break;
+                    case 2:
+                        System.out.println("========USUARIOS EN EL SISTMA=========");
+                        for (CuentaUsuario user : miMetodo.usuarios){
+                            System.out.println(user.getCorreoElectronico());
+                        }
+                        break;
+                    case 3:
+                        System.out.println("==========REPORTE DE INGRESOS============");
+                        miMetodo.Reporte();
+                        break;
+
+                }
+            }while (opcion != 3);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
